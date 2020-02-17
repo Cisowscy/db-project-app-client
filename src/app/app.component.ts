7b79710bb3a69880a826a472bbea843fdaec8e05
J.R.Cisowski Cisowscy.com <janromancisowski@cisowscy.com>
2020-02-17 06:27:34 +0100

    inicjalizacja submodułu app-client

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cisdb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+}
